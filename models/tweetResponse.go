90728cc605b2f185f9f89dbc54d93d209730f5df
keisuke watanabe <leslesnoa55@gmail.com>
2022-01-12 21:08:59 +0900

    feature_date_fix

--- models/tweetResponse.go
+++ models/tweetResponse.go
@@ -1,10 +1,14 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type TweetsResponse struct {
 	ID      primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
 	UserID  string             `bson:"user_id" json:"user_id,omitempty"`
 	Message string             `bson:"message" json:"message,omitempty"`
-	Date    string             `bson:"date" json:"date,omitempty"`
+	Date    time.Time          `bson:"date" json:"date,omitempty"`
 }
